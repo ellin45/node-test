2e9e6517b2b960c5770d01bc8afbe5db6ad96c38
ellin45 <119480528+ellin45@users.noreply.github.com>
2022-12-06 21:25:48 +0900

    node

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ app.get('/', (req, res) => {
 })
 app.get('/sound/:name', (req, res) => {
     const { name }= req.params
-    if(name == 'dog') {
+    if(name == "dog") {
         res.json({'sound' : '멍멍'})
     } else if(name == "cat") {
         res.json({'sound' : '야옹'})
-    } else if(name =='pig') {
+    } else if(name =="pig") {
         res.json({'sound' : '꿀꿀'})
     } else {
         res.json({'sound' : '알수 없음'})
